969eb423c7156fb764305ac33905b2126277a890
leonardo <canobarrera@gmail,com>
2024-10-23 18:12:52 -0600

    subir todo con la grafica

--- src/components/index.js
+++ src/components/index.js
@@ -4,3 +4,4 @@ export * from './commons/Navbar';
 export * from './commons/Title';
 export * from './commons/Menu';
 export * from './datatables/DTable';
+export * from './echarts/PieChart';
--- src/screens/Informes.jsx
+++ src/screens/Informes.jsx
@@ -1,10 +1,10 @@
-import { DTable, Footer, Menu, Navbar, Title } from "../components"
+import { DTable, Footer, Menu, Navbar, PieChart, Title } from "../components"
 
 export const Informes = () => {
     return (
         <>
             <Navbar />
-            <Menu nombre="InveCastor" usuario="Castor uwu" />
+            <Menu nombre="InventarioSys" usuario="Jaimito el Cartero" />
             <div className="content-wrapper">
                 <Title title="Informes" breadcrums={["Personas", "Menú"]} />
                 <section className="content">
@@ -18,29 +18,42 @@ export const Informes = () => {
                                 <div className="card-body">
                                     <form>
                                         <div className="form-group">
-                                            <label>Matricula/Identificador/No. de empleado</label>
-                                            <input className="form-control" placeholder="NX02154" />
+                                            <label>Empleado</label>
+                                            <select className="form-control">
+                                                <option>-Seleccione-</option>
+                                                <option>Juan perez</option>
+                                                <option>Laura Sanchez</option>
+                                            </select>
                                         </div>
+
                                         <div className="form-group">
-                                            <label>Nombre(s)</label>
-                                            <input className="form-control" placeholder="Alfredo" />
+                                            <label>Ubicación</label>
+                                            <select className="form-control">
+                                                <option>-Todas-</option>
+                                                <option>Administración</option>
+                                                <option>Recepción</option>
+                                            </select>
                                         </div>
+
+
                                         <div className="form-group">
-                                            <label>Apellido Paterno</label>
-                                            <input className="form-control" placeholder="Adame" />
+                                            <label>-Tipo de articulo-</label>
+                                            <select className="form-control">
+                                                <option>Todos los tipos</option>
+                                                <option>Muebles</option>
+                                                <option>Equipos de computo</option>
+                                                <option>Equipos de laboratorio</option>
+                                                <option>Articulo de laboratorio</option>
+                                                <option>Articulo general</option>
+                                                <option>Otro</option>
+                                            </select>
                                         </div>
+
                                         <div className="form-group">
-                                            <label>Apellido Materno</label>
-                                            <input className="form-control" placeholder="Buenrostro" />
+                                            <label>-Tipo de articulo-</label>
+                                            <input type="date" className="" />
                                         </div>
-                                        <div className="form-group">
-                                            <label>Teléfono</label>
-                                            <input className="form-control" placeholder="111222333" />
-                                        </div>
-                                        <div className="form-group">
-                                            <label>Correo electrónico</label>
-                                            <input className="form-control" placeholder="yomero@correo.net" />
-                                        </div>
+
                                     </form>
                                 </div>
                                 <div className="card-footer">
@@ -50,17 +63,43 @@ export const Informes = () => {
                             </div>
                         </div>
                         <div className="col-8">
-                            <div className="card card-primary">
-                                <div className="card-header">
-                                    <h4 className="card-title">Personas registradas</h4>
+                            <div className="row">
+                                <div className="col-12">
+                                    <div className="card card-primary">
+                                        <div className="card-header">
+                                            <h4 className="card-title">Resultados</h4>
+                                        </div>
+                                        <div className="card-body">
+
+                                        </div>
+                                    </div>
                                 </div>
-                                <div className="card-body">
-                                    <DTable />
+
+                                <div className="col-12">
+                                    <div className="card card-primary">
+                                        <div className="card-header">
+                                            <h4 className="card-title">Resultados</h4>
+                                        </div>
+                                        <div className="card-body">
+                                            <PieChart/>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div className="col-12">
+                                    <div className="card card-primary">
+                                        <div className="card-header">
+                                            <h4 className="card-title">Resultados</h4>
+                                        </div>
+                                        <div className="card-body">
+                                        <DTable />
+
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
-                        </div> 
+                        </div>
                     </div>
-
                 </section>
             </div>
             <Footer />
